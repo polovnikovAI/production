52dea677f02f6da0706b87726b1b6c2e7c80465f
Alexey Polovnikov <alexey@MacBook-Air-Alexey.local>
2024-05-24 13:49:20 +0500

    appRouter, routeConfig

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -6,6 +6,7 @@ import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
 import { MainPage } from "pages/MainPage";
 import { AboutPage } from "pages/AboutPage";
+import { AppRouter } from "./providers/router";
 
 
 
@@ -17,12 +18,7 @@ const App = () => {
             <button onClick={toggleTheme}>TOGGLE</button>
             <Link to={'/'}>Главная</Link>
             <Link to={'/about'}>О сайте</Link>
-            <Suspense fallback={<div>Loading...</div>}>
-                <Routes>
-                    <Route path={'/about'} element={<AboutPage />} />
-                    <Route path={'/'} element={<MainPage />} />
-                </Routes>
-            </Suspense>
+            <AppRouter />
         </div>
     );
 };
