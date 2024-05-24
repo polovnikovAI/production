3937d0fb24ebc3acb55606d24faa090bfc846803
Alexey Polovnikov <alexey@MacBook-Air-Alexey.local>
2024-05-24 14:39:23 +0500

    navbar, ui kit component

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,8 @@
-import { Route, Routes } from "react-router-dom";
 import './styles/index.scss'
-import { Link } from "react-router-dom";
-import { Suspense } from "react";
 import { classNames } from "shared/lib/classNames/classNames";
 import { useTheme } from "app/providers/ThemeProvider";
-import { MainPage } from "pages/MainPage";
-import { AboutPage } from "pages/AboutPage";
 import { AppRouter } from "./providers/router";
-
+import { Navbar } from "widgets/Navbar";
 
 
 const App = () => {
@@ -15,10 +10,9 @@ const App = () => {
 
     return (
         <div className={classNames('app', {}, [theme])}>
-            <button onClick={toggleTheme}>TOGGLE</button>
-            <Link to={'/'}>Главная</Link>
-            <Link to={'/about'}>О сайте</Link>
+            <Navbar />
             <AppRouter />
+            <button onClick={toggleTheme}>TOGGLE</button>
         </div>
     );
 };
