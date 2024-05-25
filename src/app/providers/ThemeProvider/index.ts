24daefa04645042c795940ec0ddf437c098d1096
Alexey Polovnikov <alexey@MacBook-Air-Alexey.local>
2024-05-25 16:28:53 +0500

    svg, file loader, button ui kit, themeSwitcher

--- src/app/providers/ThemeProvider/index.ts
+++ src/app/providers/ThemeProvider/index.ts
@@ -1,8 +1,10 @@
 import { useTheme } from "./lib/useTheme";
 import ThemeProvider from "./ui/ThemeProvider";
+import { Theme } from "./lib/ThemeContext";
 
 
 export {
     ThemeProvider,
-    useTheme
+    useTheme,
+    Theme
 };
