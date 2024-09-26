2f18ed5ae2257db938a45589ee51733832317034
polovnikovAI <head080203@gmail.com>
2024-09-26 12:01:44 +0500

    fix prettier for scss, linting scss, add translation

--- src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
+++ src/widgets/Sidebar/ui/Sidebar/Sidebar.tsx
@@ -4,6 +4,7 @@ import { useState } from 'react'
 import { ThemeSwitcher } from 'shared/ui/ThemeSwitcher'
 import { LangSwitcher } from 'shared/ui/LangSwitcher/LangSwitcher'
 import { Button } from 'shared/ui/Button/Button'
+import { useTranslation } from 'react-i18next'
 
 interface SidebarProps {
     className?: string
@@ -11,6 +12,7 @@ interface SidebarProps {
 
 export const Sidebar = ({ className }: SidebarProps) => {
     const [collapsed, setCollapsed] = useState(false)
+    const { t } = useTranslation()
 
     const onToggle = () => {
         setCollapsed((prev) => !prev)
@@ -21,11 +23,8 @@ export const Sidebar = ({ className }: SidebarProps) => {
             data-testid='sidebar'
             className={classNames(cls.Sidebar, { [cls.collapsed]: collapsed }, [className])}
         >
-            <Button
-                data-testid='sidebar-toggle'
-                onClick={onToggle}
-            >
-                toggle
+            <Button data-testid='sidebar-toggle' onClick={onToggle}>
+                {t('Переключить')}
             </Button>
             <div className={cls.switchers}>
                 <ThemeSwitcher />
