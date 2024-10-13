21017afa42a0fc293ed5fde3b6cb0d6eff06f3d8
polovnikovAI <head080203@gmail.com>
2024-10-13 14:43:02 +0500

    add redux store, add first entity

---
+++ src/entities/Counter/model/selectors/getCounterValue/getCounterValue.ts
@@ -0,0 +1,6 @@
+import { createSelector } from '@reduxjs/toolkit'
+import { StateSchema } from 'app/providers/StoreProvider'
+import { getCounter } from '../getCounter/getCounter'
+import { CounterSchema } from '../../types/counterSchema'
+
+export const getCounterValue = createSelector(getCounter, (counter: CounterSchema) => counter.value)
