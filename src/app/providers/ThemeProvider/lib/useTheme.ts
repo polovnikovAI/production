fbd2febf1a465a2085a27965fa4d7d543c937b9f
polovnikovAI <head080203@gmail.com>
2024-10-16 14:04:08 +0500

    fix global themes

--- src/app/providers/ThemeProvider/lib/useTheme.ts
+++ src/app/providers/ThemeProvider/lib/useTheme.ts
@@ -1,22 +1,33 @@
-import { useContext } from "react";
-import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from "./ThemeContext";
+import { useContext } from 'react'
+import { LOCAL_STORAGE_THEME_KEY, Theme, ThemeContext } from './ThemeContext'
 
 interface UseThemeResult {
-    toggleTheme: () => void;
+    toggleTheme: () => void
     theme: Theme
 }
 
 export function useTheme(): UseThemeResult {
-    const {theme, setTheme} = useContext(ThemeContext);
+    const { theme, setTheme } = useContext(ThemeContext)
 
     const toggleTheme = () => {
-        const newTheme = theme == Theme.DARK ? Theme.LIGHT : Theme.DARK
-        setTheme(newTheme)
+        let newTheme: Theme
+        switch (theme) {
+            case Theme.DARK:
+                newTheme = Theme.LIGHT
+                break
+            case Theme.LIGHT:
+                newTheme = Theme.DARK
+                break
+            default:
+                newTheme = Theme.LIGHT
+        }
+        setTheme?.(newTheme)
+        document.body.className = newTheme
         localStorage.setItem(LOCAL_STORAGE_THEME_KEY, newTheme)
     }
 
     return {
         theme,
-        toggleTheme
+        toggleTheme,
     }
 }
