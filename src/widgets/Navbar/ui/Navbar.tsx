712e1feeff700b57fac624cd6231c799b73f9672
polovnikovAI <head080203@gmail.com>
2024-10-16 16:07:58 +0500

    add new input, add login, form ui

--- src/widgets/Navbar/ui/Navbar.tsx
+++ src/widgets/Navbar/ui/Navbar.tsx
@@ -3,7 +3,7 @@ import { classNames } from 'shared/lib/classNames/classNames'
 import cls from './Navbar.module.scss'
 import { useTranslation } from 'react-i18next'
 import { Button, ButtonTheme } from 'shared/ui/Button/Button'
-import { Modal } from 'shared/ui/Modal/Modal'
+import { LoginModal } from 'features/AuthByUsername'
 
 interface NavbarProps {
     className?: string
@@ -14,24 +14,20 @@ export const Navbar: FC<NavbarProps> = () => {
 
     const [isAuthModal, setIsAuthModal] = useState(false)
 
-    const onToggleModal = useCallback(() => {
-        setIsAuthModal((prev) => !prev)
+    const onCloseModal = useCallback(() => {
+        setIsAuthModal(false)
+    }, [])
+
+    const onShowModal = useCallback(() => {
+        setIsAuthModal(true)
     }, [])
 
     return (
         <div className={classNames(cls.Navbar, {}, [])}>
-            <Button
-                theme={ButtonTheme.CLEAR_INVERTED}
-                className={cls.links}
-                onClick={onToggleModal}
-            >
+            <Button theme={ButtonTheme.CLEAR_INVERTED} className={cls.links} onClick={onShowModal}>
                 {t('Войти')}
             </Button>
-            <Modal isOpen={isAuthModal} onClose={onToggleModal}>
-                Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aliquid, itaque ratione
-                exercitationem, qui officiis aperiam iste dolor maiores, illum tempora fugit dolores
-                debitis dolorem culpa? Consequatur, velit odit. Cum, tenetur?
-            </Modal>
+            <LoginModal isOpen={isAuthModal} onClose={onCloseModal} />
         </div>
     )
 }
